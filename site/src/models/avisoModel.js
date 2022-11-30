4eabb039784c8288ba6100c06cfa7e30d2db0b1b
WashingtonFarias <washington.farias@sptech.school>
2022-11-30 15:06:05 -0300

    Arrumando o select

--- site/src/models/avisoModel.js
+++ site/src/models/avisoModel.js
@@ -92,7 +92,7 @@ function deletar(idAviso) {
 function nota() {
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
-        select nome, notas from quiz order by notas limit 10;
+        select nome, notas from quiz order by notas desc limit 10;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
